15cbeb33b478b381df5ca70f8390bf448176a78e
Faber Vergara <fabervergara@gmail.com>
2022-01-12 18:24:51 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,28 +4,27 @@
       <h1 class="text-3xl">Search by Faber Vergara</h1>
     </div>
     <div class="flex justify-center mt-12">
-      <form @submit="getBooks">
-        <input
-          type="text"
-          class="border border-gray-700 focus:border-blue-400 rounded h-10 w-64"
-          placeholder="Type a book name"
-          v-model="search"
-        />
-        <button
-          class="bg-blue-600 text-white h-10 w-32 rounded disabled:bg-gray-600"
-          :disabled="isButtonDisabled"
-          type="submit"
-        >
-          Search
-        </button>
-      </form>
+      <input
+        type="text"
+        class="border border-gray-700 focus:border-blue-400 rounded h-10 w-64"
+        placeholder="Type a book name"
+        v-model="search"
+      />
+      <button
+        class="bg-blue-600 text-white h-10 w-32 rounded disabled:bg-gray-600"
+        :disabled="isButtonDisabled"
+        @click="$fetch"
+      >
+        Search
+      </button>
     </div>
     <div class="flex justify-center mt-8">
       <ul class="list-disc">
         <li v-for="book in books.docs" :key="book.key">{{ book.title }}</li>
       </ul>
 
-      <p v-if="loading">Loading</p>
+      <p v-if="$fetchState.pending">Loading</p>
+      <p v-else-if="$fetchState.error">An error occurred :(</p>
     </div>
   </div>
 </template>
@@ -44,21 +43,10 @@ export default {
       return this.search === "";
     },
   },
-  methods: {
-    getBooks(e) {
-      e.preventDefault();
-      let self = this;
-      this.loading = true;
-
-      this.$axios
-        .get(
-          "http://openlibrary.org/search.json?q=" + this.search + "&limit=20"
-        )
-        .then((res) => {
-          self.books = res.data;
-          self.loading = false;
-        });
-    },
+  async fetch() {
+    this.books = await fetch(
+      "http://openlibrary.org/search.json?q=" + this.search + "&limit=20"
+    ).then((res) => res.json());
   },
 };
 </script>
